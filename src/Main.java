3821cff2ef32071f739481c6646c5e9e681dc707
DAP2106 <pachkovskiid82@gmail.com>
2023-04-05 20:37:35 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ public class Main {
 
         int check = 100;
         int deposit = 1100;
-        int bonus ;
+        int bonus;
 
-        if ( deposit  > 1000) {bonus = deposit / 100 ; } else {bonus = 0;}
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
 
         int balance = (check + deposit + bonus);
 
         System.out.println("Ваш бонус " + bonus + " рублей   ");
-        System.out.println("Ваш баланс "   + balance + "  Рублей  ");
+        System.out.println("Ваш баланс " + balance + "  Рублей  ");
     }
 }
